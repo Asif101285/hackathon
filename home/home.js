7a5e6dba89a0216525578cb80c949318f915b6df
Mohammad Asif <asifmajeed1072@gmail.com>
2023-08-19 19:52:41 +0500

    complete work

--- home/home.js
+++ home/home.js
@@ -56,6 +56,79 @@ onAuthStateChanged(auth, (user) => {
             createdAt: serverTimestamp(),
           });
 
+          window.addEventListener("load", () => {
+            const q = query(collection(db, currentUserUID), orderBy("createdAt", "desc"));
+            const blogSection = document.querySelector("#blogSection");
+            const unsubscribe = onSnapshot(q, (querySnapshot) => {
+              blogSection.innerHTML = "";
+              querySnapshot.forEach((doc) => {
+                const post = document.createElement("div");
+                // post.innerText = doc.data().inputText;
+                post.classList.add("post");
+                post.id = doc.data().id;
+                const title = document.createElement("div");
+                title.classList.add("title");
+                title.innerText = doc.data().title;
+                const inputText = document.createElement("div");
+                inputText.classList.add("inputText");
+                inputText.innerText = doc.data().inputText;
+          
+                const deleteBtn = document.createElement("button");
+                deleteBtn.classList.add("btn");
+                deleteBtn.id = `${doc.data().id}`;
+                deleteBtn.innerText = "Delete";
+          
+                const editBtn = document.createElement("button");
+                editBtn.classList.add("btn");
+                editBtn.id = `${doc.id}`;
+                editBtn.innerText = "Edit";
+                post.appendChild(title);
+                post.appendChild(inputText);
+                post.appendChild(deleteBtn);
+                post.appendChild(editBtn);
+                blogSection.appendChild(post);
+                deleteBtn.addEventListener("click", () =>
+                  deletePostFunc(doc.id, doc.data().title)
+                );
+                //   editBtn.addEventListener("click", () =>
+                //     editPostFunc(doc.id, doc.data().inputText)
+                //   );
+              });
+            });
+          });
+          
+          const deletePostFunc = async (id, globalId) => {
+            try {
+              // Delete from user's personal collection
+              await deleteDoc(doc(db, currentUserUID, id));
+          
+              // Delete from global collection
+              const globalQuerySnapshot = await getDocs(
+                query(collection(db, "global"), where("title", "==", globalId))
+              );
+              globalQuerySnapshot.forEach(async (doc) => {
+                if (doc.data().title === globalId) {
+                  await deleteDoc(doc.ref);
+                }
+              });
+          
+              console.log("Document deleted successfully");
+            } catch (error) {
+              console.error("Error deleting document: ", error);
+            }
+          };
+          
+          const generateUniqueId = () => {
+            const characters =
+              "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const length = 10;
+            let uniqueId = "";
+            for (let i = 0; i < length; i++) {
+              const randomIndex = Math.floor(Math.random() * characters.length);
+              uniqueId += characters.charAt(randomIndex);
+            }
+            return `id-${uniqueId}`;
+          };
 
 
         console.log("Document written with ID: ", docRef.id);
